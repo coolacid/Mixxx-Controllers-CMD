e4500cb03060fbd10b8bdba0699a01b2d81bc615
CoolAcid <coolacid@gmail.com>
2017-02-08 13:01:00 -0500

    Map lights and set them

--- CMD_PL-1_MIDI_1.midi.js
+++ CMD_PL-1_MIDI_1.midi.js
@@ -17,29 +17,42 @@ function BehringerCMDPL1() {};
 BehringerCMDPL1.initLEDs = function () {
     // (re)Initialise any LEDs that are direcctly controlled by this script.
     print("CMD PL-1: Setting LEDs");
-    midi.sendShortMsg(0xB4, 80, 48); // VuMeterL
-    midi.sendShortMsg(0xB4, 81, 48); // VuMeterR
 
-    midi.sendShortMsg(0x94, 0x12, 0x00); // Middle Button
+    // Loop through each channel since the PL-1 supports 4 different decks
+    for (Channel=0; Channel <= 3; Channel++) {
+        print ("CMD PL-1: Setting Channel: " + (Channel+1));
+        for (Encoder=0; Encoder <=7; Encoder++) {
+            // Loop through each encoder to set to mid section
+            midi.sendShortMsg(0xB0 + Channel, 00+Encoder, 8);
+        }
 
-//    midi.sendShortMsg(0x94, 0x10, 0x01); // Left Button  // Doesn't Work?
-//    midi.sendShortMsg(0x94, 0x11, 0x01); // Right Button // Doesn't Work?
+        // Set the rest of the LEDs
+        midi.sendShortMsg(0xB0 + Channel, 10, 8); // Pitch
 
-    midi.sendShortMsg(0x94, 0x13, 0x00); // Channel 1, 1
-    midi.sendShortMsg(0x94, 0x14, 0x00); // Channel 1, 2
-    midi.sendShortMsg(0x94, 0x30, 0x00); // Channel 1, Que
+        midi.sendShortMsg(0x90 + Channel, 0x10, 0x00); // Button: 1
+        midi.sendShortMsg(0x90 + Channel, 0x11, 0x00); // Button: 2
+        midi.sendShortMsg(0x90 + Channel, 0x12, 0x00); // Button: 3
+        midi.sendShortMsg(0x90 + Channel, 0x13, 0x00); // Button: 4
+        midi.sendShortMsg(0x90 + Channel, 0x14, 0x00); // Button: 5
+        midi.sendShortMsg(0x90 + Channel, 0x15, 0x00); // Button: 6
+        midi.sendShortMsg(0x90 + Channel, 0x16, 0x00); // Button: 7
+        midi.sendShortMsg(0x90 + Channel, 0x17, 0x00); // Button: 8
 
-    midi.sendShortMsg(0x94, 0x17, 0x00); // Channel 2, 1
-    midi.sendShortMsg(0x94, 0x18, 0x00); // Channel 2, 2
-    midi.sendShortMsg(0x94, 0x31, 0x00); // Channel 2, Que
+        midi.sendShortMsg(0x90 + Channel, 0x18, 0x00); // Button: Load
+        midi.sendShortMsg(0x90 + Channel, 0x19, 0x00); // Button: Lock
 
-    midi.sendShortMsg(0x94, 0x1B, 0x00); // Channel 3, 1
-    midi.sendShortMsg(0x94, 0x1C, 0x00); // Channel 3, 2
-    midi.sendShortMsg(0x94, 0x32, 0x00); // Channel 3, Que
+        midi.sendShortMsg(0x90 + Channel, 0x1B, 0x00); // Button: Scratch
+        midi.sendShortMsg(0x90 + Channel, 0x20, 0x00); // Button: Sync
+        midi.sendShortMsg(0x90 + Channel, 0x21, 0x00); // Button: Tap
+        midi.sendShortMsg(0x90 + Channel, 0x22, 0x00); // Button: Cue
+        midi.sendShortMsg(0x90 + Channel, 0x23, 0x00); // Button: Play
 
-    midi.sendShortMsg(0x94, 0x1F, 0x00); // Channel 4, 1
-    midi.sendShortMsg(0x94, 0x20, 0x00); // Channel 4, 2
-    midi.sendShortMsg(0x94, 0x33, 0x00); // Channel 4, Que
+        midi.sendShortMsg(0x90 + Channel, 0x24, 0x00); // Button: <<
+        midi.sendShortMsg(0x90 + Channel, 0x25, 0x00); // Button: >>
+        midi.sendShortMsg(0x90 + Channel, 0x26, 0x00); // Button: -
+        midi.sendShortMsg(0x90 + Channel, 0x27, 0x00); // Button: +
+
+    }
 }
 
 BehringerCMDPL1.init = function () {
@@ -49,5 +62,5 @@ BehringerCMDPL1.init = function () {
 
 BehringerCMDPL1.shutdown = function () {
     // Reset the Lights to off
-//    BehringerCMDMM1.initLEDs();
+    BehringerCMDMM1.initLEDs();
 };
